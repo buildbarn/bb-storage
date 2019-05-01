f7a9d861d78f317fdcff1a8dc3aa169a567bd004
Aleksey Pesternikov <apesternikov@gmail.com>
2019-05-01 12:39:10 -0700

    ability to change registry/repo/tag

--- tools/container.bzl
+++ tools/container.bzl
@@ -5,7 +5,7 @@ def container_push_official(name, image, component):
         name = name,
         format = "Docker",
         image = image,
-        registry = "index.docker.io",
-        repository = "buildbarn/" + component,
-        tag = "{BUILD_SCM_TIMESTAMP}-{BUILD_SCM_REVISION}",
+        registry = "{BB_REGISTRY}",
+        repository = "{BB_REPO}" + component,
+        tag = "{BB_TAG}",
     )
--- tools/workspace-status.sh
+++ tools/workspace-status.sh
@@ -1,6 +1,12 @@
 #!/bin/sh -e
-
+BUILD_SCM_REVISION=$(git rev-parse --short HEAD)
+BUILD_SCM_TIMESTAMP=$(TZ=UTC date --date "@$(git show -s --format=%ct HEAD)" +%Y%m%dT%H%M%SZ)
+BB_DEFAULT_TAG='dev'
 if git diff-index --quiet master --; then
-  echo "BUILD_SCM_REVISION $(git rev-parse --short HEAD)"
-  echo "BUILD_SCM_TIMESTAMP $(TZ=UTC date --date "@$(git show -s --format=%ct HEAD)" +%Y%m%dT%H%M%SZ)"
+  echo "BUILD_SCM_REVISION ${BUILD_SCM_REVISION}"
+  echo "BUILD_SCM_TIMESTAMP ${BUILD_SCM_TIMESTAMP}"
+  BB_DEFAULT_TAG="${BUILD_SCM_TIMESTAMP}-${BUILD_SCM_REVISION}"
 fi
+echo BB_REGISTRY ${BB_REGISTRY_OVERRIDE:-index.docker.io}
+echo BB_REPO ${BB_REPO_OVERRIDE:-buildbarn/}
+echo BB_TAG ${BB_TAG_OVERRIDE:-${BB_DEFAULT_TAG}}
