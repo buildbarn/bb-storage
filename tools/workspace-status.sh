182e1df5cdc3affab9a053c3d8667425e0f8fb2b
Mickael Carl <contact+git@carlm.org>
2019-07-27 16:59:17 +0200

    Allow pushing images when in detached HEAD

--- tools/workspace-status.sh
+++ tools/workspace-status.sh
@@ -1,6 +1,6 @@
 #!/bin/sh -e
 
-if git diff-index --quiet master --; then
+if git diff-index --quiet origin/master --; then
   echo "BUILD_SCM_REVISION $(git rev-parse --short HEAD)"
   echo "BUILD_SCM_TIMESTAMP $(TZ=UTC date --date "@$(git show -s --format=%ct HEAD)" +%Y%m%dT%H%M%SZ)"
 fi
