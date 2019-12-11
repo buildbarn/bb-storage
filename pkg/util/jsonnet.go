fdcebf2e04077a7c206ba7c88d6bda6f72dea774
Ed Schouten <eschouten@apple.com>
2019-12-11 06:18:22 -0800

    Jsonnet: make the environment available through std.extVar()
    It may sometimes be useful to be able to pass in configuration options
    of Buildbarn binaries through environment variables. Examples include
    running Buildbarn on top of Kubernetes, where you want to use the name
    of a pod or node in the configuration. Kubernetes can be configured to
    pass those in as environment variables. These can now be extracted using
    std.extVar().

--- pkg/util/jsonnet.go
+++ pkg/util/jsonnet.go
@@ -2,27 +2,47 @@ package util
 
 import (
 	"io/ioutil"
+	"os"
+	"strings"
 
 	"github.com/golang/protobuf/jsonpb"
 	"github.com/golang/protobuf/proto"
-	jsonnet "github.com/google/go-jsonnet"
+	"github.com/google/go-jsonnet"
 	"github.com/google/go-jsonnet/ast"
 	"github.com/google/go-jsonnet/astgen"
+
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 )
 
 func init() {
 	ast.StdAst = astgen.StdAst
 }
 
-// UnmarshalConfigurationFromFile reads a jsonnet file, renders it, unmarshal
-// the output into a protobuf configuration message.
+// UnmarshalConfigurationFromFile reads a Jsonnet file, evaluates it and
+// unmarshals the output into a Protobuf message.
 func UnmarshalConfigurationFromFile(path string, configuration proto.Message) error {
-	vm := jsonnet.MakeVM()
 	jsonnetInput, err := ioutil.ReadFile(path)
 	if err != nil {
-		return StatusWrapf(err, "Failed to run Jsonnet on %s", path)
+		return StatusWrapf(err, "Failed to read file contents")
 	}
+
+	// Create a Jsonnet VM where all of the environment variables of
+	// the current process are available through std.extVar().
+	vm := jsonnet.MakeVM()
+	for _, env := range os.Environ() {
+		parts := strings.SplitN(env, "=", 2)
+		if len(parts) != 2 {
+			return status.Errorf(codes.InvalidArgument, "Invalid environment variable: %#v", env)
+		}
+		vm.ExtVar(parts[0], parts[1])
+	}
+
 	jsonnetOutput, err := vm.EvaluateSnippet(path, string(jsonnetInput))
+	if err != nil {
+		return StatusWrapf(err, "Failed to evaluate configuration")
+	}
+
 	if err := jsonpb.UnmarshalString(jsonnetOutput, configuration); err != nil {
 		return StatusWrap(err, "Failed to unmarshal configuration")
 	}
