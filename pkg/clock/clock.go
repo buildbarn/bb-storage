71284d9b12058d4a7efad152a97e70fa4897c044
Ed Schouten <eschouten@apple.com>
2019-12-11 06:13:56 -0800

    Add an interface for stubbing out clock access
    There are places in the codebase where we need to access the clock,
    either for obtaining the time or to wait on it. This change introduces a
    simple interface for the clock. By using this consistently, we can
    easily test code that is time sensitive without any slowdown/races.

---
+++ pkg/clock/clock.go
@@ -0,0 +1,30 @@
+package clock
+
+import (
+	"context"
+	"time"
+)
+
+// Clock is an interface around some of the standard library functions
+// that provide time handling. It has been added to aid unit testing.
+type Clock interface {
+	// Return the current time of day. Equivalent to time.Now().
+	Now() time.Time
+
+	// Create a Context object that automatically cancels after a
+	// certain amount of time has passed. Equivalent to
+	// context.WithTimeout().
+	NewContextWithTimeout(parent context.Context, timeout time.Duration) (context.Context, context.CancelFunc)
+
+	// Create a channel that publishes the time of day at a point of
+	// time in the future. Unlike time.NewTimer(), this function
+	// returns the channel directly to allow Timer to be an
+	// interface.
+	NewTimer(d time.Duration) (Timer, <-chan time.Time)
+}
+
+// Timer is an interface around time.Timer. It has been added to aid
+// unit testing.
+type Timer interface {
+	Stop() bool
+}
---
+++ pkg/clock/system_clock.go
@@ -0,0 +1,25 @@
+package clock
+
+import (
+	"context"
+	"time"
+)
+
+type systemClock struct{}
+
+func (c systemClock) Now() time.Time {
+	return time.Now()
+}
+
+func (c systemClock) NewContextWithTimeout(parent context.Context, timeout time.Duration) (context.Context, context.CancelFunc) {
+	return context.WithTimeout(parent, timeout)
+}
+
+func (c systemClock) NewTimer(d time.Duration) (Timer, <-chan time.Time) {
+	t := time.NewTimer(d)
+	return t, t.C
+}
+
+// SystemClock is a Clock that corresponds to the current time of day,
+// as reported by the operating system.
+var SystemClock Clock = systemClock{}
