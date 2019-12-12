dd4ae3b75d4626503bf8cffd2aa382dbbb7fa5c3
Ed Schouten <eschouten@apple.com>
2019-12-11 22:09:35 -0800

    Add a unified API for creating gRPC clients and servers
    Introduce a common Protobuf message for configuring gRPC clients and
    servers. Servers can listen on multipel TCP and UNIX socket addresses.
    They may use TLS with optional client certificate authentication.
    
    The goal is that we're going to use these functions uniformally
    throughout the Buildbarn codebase, so that all of our components have
    the same set of features when it comes to using gRPC.

---
+++ pkg/grpc/authenticator.go
@@ -0,0 +1,77 @@
+package grpc
+
+import (
+	"context"
+	"crypto/x509"
+
+	"github.com/buildbarn/bb-storage/pkg/clock"
+	configuration "github.com/buildbarn/bb-storage/pkg/proto/configuration/grpc"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+)
+
+// Authenticator can be used to grant or deny access to a gRPC server.
+// Implementations may grant access based on TLS connection state,
+// provided headers, source IP address ranges, etc. etc. etc.
+type Authenticator interface {
+	Authenticate(ctx context.Context) error
+}
+
+// NewAuthenticatorFromConfiguration creates a tree of Authenticator
+// objects based on a configuration file.
+func NewAuthenticatorFromConfiguration(policy *configuration.AuthenticationPolicy) (Authenticator, error) {
+	if policy == nil {
+		return nil, status.Error(codes.InvalidArgument, "Authentication policy not specified")
+	}
+	switch policyKind := policy.Policy.(type) {
+	case *configuration.AuthenticationPolicy_Allow:
+		return AllowAuthenticator, nil
+	case *configuration.AuthenticationPolicy_Any:
+		children := make([]Authenticator, 0, len(policyKind.Any.Policies))
+		for _, childConfiguration := range policyKind.Any.Policies {
+			child, err := NewAuthenticatorFromConfiguration(childConfiguration)
+			if err != nil {
+				return nil, err
+			}
+			children = append(children, child)
+		}
+		return NewAnyAuthenticator(children), nil
+	case *configuration.AuthenticationPolicy_TlsClientCertificate:
+		clientCAs := x509.NewCertPool()
+		if !clientCAs.AppendCertsFromPEM([]byte(policyKind.TlsClientCertificate.ClientCertificateAuthorities)) {
+			return nil, status.Error(codes.InvalidArgument, "Failed to parse client certificate authorities")
+		}
+		return NewTLSClientCertificateAuthenticator(
+			clientCAs,
+			clock.SystemClock), nil
+	default:
+		return nil, status.Error(codes.InvalidArgument, "Configuration did not contain an authentication policy type")
+	}
+}
+
+// NewAuthenticatingUnaryInterceptor creates a gRPC request interceptor
+// for unary calls that passes all requests through an Authenticator.
+// This may be used to enable authentication support on a gRPC server.
+func NewAuthenticatingUnaryInterceptor(a Authenticator) grpc.UnaryServerInterceptor {
+	return func(ctx context.Context, req interface{}, info *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (resp interface{}, err error) {
+		if err := a.Authenticate(ctx); err != nil {
+			return nil, err
+		}
+		return handler(ctx, req)
+	}
+}
+
+// NewAuthenticatingStreamInterceptor creates a gRPC request interceptor
+// for streaming calls that passes all requests through an
+// Authenticator. This may be used to enable authentication support on a
+// gRPC server.
+func NewAuthenticatingStreamInterceptor(a Authenticator) grpc.StreamServerInterceptor {
+	return func(srv interface{}, ss grpc.ServerStream, info *grpc.StreamServerInfo, handler grpc.StreamHandler) error {
+		if err := a.Authenticate(ss.Context()); err != nil {
+			return err
+		}
+		return handler(srv, ss)
+	}
+}
---
+++ pkg/grpc/grpc.go
@@ -0,0 +1,112 @@
+package grpc
+
+import (
+	"net"
+	"os"
+
+	configuration "github.com/buildbarn/bb-storage/pkg/proto/configuration/grpc"
+	"github.com/buildbarn/bb-storage/pkg/util"
+	"github.com/grpc-ecosystem/go-grpc-middleware"
+	"github.com/grpc-ecosystem/go-grpc-prometheus"
+	"github.com/prometheus/client_golang/prometheus"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/credentials"
+	"google.golang.org/grpc/status"
+
+	"go.opencensus.io/plugin/ocgrpc"
+)
+
+// NewGRPCClientFromConfiguration creates a gRPC client based on a
+// configuration stored in a Protobuf message. This Protobuf message is
+// used within all configuration files of Buildbarn applications.
+func NewGRPCClientFromConfiguration(configuration *configuration.GRPCClientConfiguration) (*grpc.ClientConn, error) {
+	if configuration == nil {
+		return nil, status.Error(codes.InvalidArgument, "No gRPC client configuration provided")
+	}
+
+	tlsConfig, err := util.NewTLSConfigFromClientConfiguration(configuration.Tls)
+	if err != nil {
+		return nil, util.StatusWrap(err, "Failed to create TLS configuration")
+	}
+	var securityOption grpc.DialOption
+	if tlsConfig != nil {
+		securityOption = grpc.WithTransportCredentials(credentials.NewTLS(tlsConfig))
+	} else {
+		securityOption = grpc.WithInsecure()
+	}
+
+	return grpc.Dial(
+		configuration.Address,
+		securityOption,
+		grpc.WithUnaryInterceptor(grpc_prometheus.UnaryClientInterceptor),
+		grpc.WithStreamInterceptor(grpc_prometheus.StreamClientInterceptor))
+}
+
+// NewGRPCServersFromConfigurationAndServe creates a series of gRPC
+// servers based on a configuration stored in a list of Protobuf
+// messages. In then lets all of these gRPC servers listen on the
+// network addresses of UNIX socket paths provided.
+func NewGRPCServersFromConfigurationAndServe(configurations []*configuration.GRPCServerConfiguration, registrationFunc func(*grpc.Server)) error {
+	serveErrors := make(chan error)
+	for _, configuration := range configurations {
+		// Create an authenticator for requests.
+		authenticator, err := NewAuthenticatorFromConfiguration(configuration.AuthenticationPolicy)
+		if err != nil {
+			return err
+		}
+
+		// Default server options.
+		serverOptions := []grpc.ServerOption{
+			grpc.UnaryInterceptor(grpc_middleware.ChainUnaryServer(
+				grpc_prometheus.UnaryServerInterceptor,
+				NewAuthenticatingUnaryInterceptor(authenticator))),
+			grpc.StreamInterceptor(grpc_middleware.ChainStreamServer(
+				grpc_prometheus.StreamServerInterceptor,
+				NewAuthenticatingStreamInterceptor(authenticator))),
+			grpc.StatsHandler(&ocgrpc.ServerHandler{}),
+		}
+
+		// Enable TLS if provided.
+		if tlsConfig, err := util.NewTLSConfigFromServerConfiguration(configuration.Tls); err != nil {
+			return err
+		} else if tlsConfig != nil {
+			serverOptions = append(serverOptions, grpc.Creds(credentials.NewTLS(tlsConfig)))
+		}
+
+		if maxRecvMsgSize := configuration.MaximumReceivedMessageSizeBytes; maxRecvMsgSize != 0 {
+			serverOptions = append(serverOptions, grpc.MaxRecvMsgSize(int(maxRecvMsgSize)))
+		}
+
+		// Create server.
+		s := grpc.NewServer(serverOptions...)
+		registrationFunc(s)
+
+		// Add Prometheus timing metrics.
+		grpc_prometheus.EnableHandlingTimeHistogram(grpc_prometheus.WithHistogramBuckets(prometheus.DefBuckets))
+		grpc_prometheus.Register(s)
+
+		// TCP sockets.
+		for _, listenAddress := range configuration.ListenAddresses {
+			sock, err := net.Listen("tcp", listenAddress)
+			if err != nil {
+				return util.StatusWrapf(err, "Failed to create listening socket for %#v", listenAddress)
+			}
+			go func() { serveErrors <- s.Serve(sock) }()
+		}
+
+		// UNIX sockets.
+		for _, listenPath := range configuration.ListenPaths {
+			if err := os.Remove(listenPath); err != nil && !os.IsNotExist(err) {
+				return util.StatusWrapf(err, "Could not remove stale socket %#v", listenPath)
+			}
+			sock, err := net.Listen("unix", listenPath)
+			if err != nil {
+				return util.StatusWrapf(err, "Failed to create listening socket for %#v", listenPath)
+			}
+			go func() { serveErrors <- s.Serve(sock) }()
+		}
+	}
+	return <-serveErrors
+}
