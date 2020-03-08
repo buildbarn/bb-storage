888f2fb90dc664f59e34fba0824d3652992132f2
Ed Schouten <eschouten@apple.com>
2020-03-08 20:40:24 +0100

    Automatically push container images to Docker Hub
    Every commit in master will now trigger an automated build that pushes
    images to Docker Hub. This should make it a lot easier for our users to
    use the container images after they send in patches.

--- tools/workspace-status.sh
+++ tools/workspace-status.sh
@@ -1,6 +1,6 @@
 #!/bin/sh -e
 
-if test "${BUILDKITE_BRANCH}" = "master"; then
+if test "${GITHUB_ACTIONS}" = "true"; then
   echo "BUILD_SCM_REVISION $(git rev-parse --short HEAD)"
   echo "BUILD_SCM_TIMESTAMP $(TZ=UTC date --date "@$(git show -s --format=%ct HEAD)" +%Y%m%dT%H%M%SZ)"
 fi
