d6f480892ea576d878bce14d896654a7bb4587fe
Scott Clarke <scott.clarke@codethink.co.uk>
2020-04-24 12:00:54 +0100

    Add Mknod() to filesystem.Directory
    Allows Mknod to be called on linux, on darwin this will give a fixed
    error as this operation will not work

--- pkg/filesystem/local_directory_linux.go
+++ pkg/filesystem/local_directory_linux.go
@@ -2,5 +2,31 @@
 
 package filesystem
 
+import (
+	"os"
+	"runtime"
+
+	"golang.org/x/sys/unix"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+)
+
 // deviceNumber is the equivalent of POSIX dev_t.
 type deviceNumber = uint64
+
+func (d *localDirectory) Mknod(name string, perm os.FileMode, dev int) error {
+	if err := validateFilename(name); err != nil {
+		return err
+	}
+	defer runtime.KeepAlive(d)
+
+	var unixPerm uint32
+	switch perm & os.ModeType {
+	case os.ModeDevice | os.ModeCharDevice:
+		unixPerm = uint32(unix.S_IFCHR | (perm & os.ModePerm))
+	default:
+		return status.Error(codes.InvalidArgument, "The provided file mode is not for a character device")
+	}
+
+	return unix.Mknodat(d.fd, name, unixPerm, dev)
+}
