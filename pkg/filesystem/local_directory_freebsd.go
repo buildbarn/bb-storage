3d6325dd890897de63aa3740b46ec275d345adbe
Ed Schouten <eschouten@apple.com>
2020-12-19 15:13:31 +0100

    Unbreak the build on FreeBSD

--- pkg/filesystem/local_directory_freebsd.go
+++ pkg/filesystem/local_directory_freebsd.go
@@ -5,6 +5,8 @@ package filesystem
 import (
 	"os"
 
+	"github.com/buildbarn/bb-storage/pkg/filesystem/path"
+
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
 )
@@ -12,7 +14,7 @@ import (
 // deviceNumber is the equivalent of POSIX dev_t.
 type deviceNumber = uint64
 
-func (d *localDirectory) Mknod(name string, perm os.FileMode, dev int) error {
+func (d *localDirectory) Mknod(name path.Component, perm os.FileMode, dev int) error {
 	// Though mknodat() exists on FreeBSD, device nodes created
 	// outside of devfs are non-functional.
 	return status.Error(codes.Unimplemented, "Creation of device nodes is not supported on FreeBSD")
