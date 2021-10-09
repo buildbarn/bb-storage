bbda328b3f7b7e9e9e564ebc151f307d335f879c
Ed Schouten <eschouten@apple.com>
2021-10-09 08:33:10 +0200

    Make the HTTP client for AWS sessions configurable
    We now have this generic way of configuring HTTP clients. Let's make
    proper use of it now.

--- pkg/cloud/aws/config.go
+++ pkg/cloud/aws/config.go
@@ -2,11 +2,14 @@ package aws
 
 import (
 	"context"
+	"net/http"
 
 	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/config"
 	"github.com/aws/aws-sdk-go-v2/credentials"
+	bb_http "github.com/buildbarn/bb-storage/pkg/http"
 	aws_pb "github.com/buildbarn/bb-storage/pkg/proto/configuration/cloud/aws"
+	"github.com/buildbarn/bb-storage/pkg/util"
 )
 
 // NewConfigFromConfiguration creates a new AWS SDK config object based
@@ -14,7 +17,13 @@ import (
 // resulting session object can be used to access AWS services such as
 // EC2, S3 and SQS.
 func NewConfigFromConfiguration(configuration *aws_pb.SessionConfiguration) (aws.Config, error) {
-	loadOptions := []func(*config.LoadOptions) error{}
+	roundTripper, err := bb_http.NewRoundTripperFromConfiguration(configuration.GetHttpClient())
+	if err != nil {
+		return aws.Config{}, util.StatusWrap(err, "Failed to create HTTP client")
+	}
+	loadOptions := []func(*config.LoadOptions) error{
+		config.WithHTTPClient(&http.Client{Transport: roundTripper}),
+	}
 	if region := configuration.GetRegion(); region != "" {
 		loadOptions = append(loadOptions, config.WithRegion(region))
 	}
