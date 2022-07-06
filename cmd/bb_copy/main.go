81407c8ba4eadaf53e13c21af5203007c38ceba6
Ed Schouten <eschouten@apple.com>
2022-07-06 11:33:21 +0200

    Add bb_copy: a generic blobstore copy utility
    By now the blobstore layer has a very large number of BlobAccess storage
    backends. We also have the BlobReplicator API that can be used to
    replicate data from one backend to another.
    
    One shortcoming is that BlobReplicator can now only be used in response
    to REv2 storage traffic (e.g., part of MirroredBlobAccess). There is no
    easy way to use BlobReplicator to do one-off copies of data. This change
    attempts to address this by adding a simple command line tool for this.
    
    There are various ways in which this tool can be used. For example,
    copying an REv2 Directory including all of its children between two
    clusters is as simple as invoking it with the following configuration:
    
        {
          source: { grpc: { address: 'cluster1.example.com:443' } },
          sink: { grpc: { address: 'cluster2.example.com:443' } },
          replicator: { 'local': {} },
          maximumMessageSizeBytes: 16 * 1024 * 1024,
          directories: [{
            hash: '3842e8454e054af62029d3bd2cb82d4ddeaf24a881c500e25440b54114f32dc2',
            sizeBytes: 710,
          }],
          traversalConcurrency: 10,
        }
    
    With the recently added ZIPWritingBlobAccess backend you can also use it
    to archive actions:
    
        {
          source: { grpc: { address: 'cluster1.example.com:443' } },
          sink: { zipWriting: { path: '/Users/ed/action.zip' } },
          replicator: { 'local': {} },
          maximumMessageSizeBytes: 16 * 1024 * 1024,
          directories: [{
            hash: '3842e8454e054af62029d3bd2cb82d4ddeaf24a881c500e25440b54114f32dc2',
            sizeBytes: 710,
          }],
          traversalConcurrency: 10,
        }
    
    Conversely, restoring data from a ZIP file can be done using
    ZIPReadingBlobAccess:
    
        {
          source: { zipReading: { path: '/Users/ed/action.zip' } },
          sink: { grpc: { address: 'cluster2.example.com:443' } },
          replicator: { 'local': {} },
          maximumMessageSizeBytes: 16 * 1024 * 1024,
          directories: [{
            hash: '3842e8454e054af62029d3bd2cb82d4ddeaf24a881c500e25440b54114f32dc2',
            sizeBytes: 710,
          }],
          traversalConcurrency: 10,
        }

---
+++ cmd/bb_copy/main.go
@@ -0,0 +1,131 @@
+package main
+
+import (
+	"context"
+	"log"
+	"os"
+
+	blobstore_configuration "github.com/buildbarn/bb-storage/pkg/blobstore/configuration"
+	"github.com/buildbarn/bb-storage/pkg/blobstore/replication"
+	"github.com/buildbarn/bb-storage/pkg/digest"
+	"github.com/buildbarn/bb-storage/pkg/grpc"
+	"github.com/buildbarn/bb-storage/pkg/proto/configuration/bb_copy"
+	"github.com/buildbarn/bb-storage/pkg/util"
+
+	"golang.org/x/sync/errgroup"
+)
+
+// A generic utility for copying data between storage backends. This
+// utility is similar to bb_replicator, in that it uses the
+// BlobReplicator layer to copy data between backends.
+//
+// The difference is that bb_replicator accepts requests of objects to
+// copy through gRPC, while this utility accepts a list of digests in
+// its configuration file, terminating as soon as replication is
+// completed.
+//
+// When used in combination with ZIPReadingBlobAccess and
+// ZIPWritingBlobAccess, this tool can also be used to backup and
+// restore parts of the Content Addressable Storage.
+
+func main() {
+	if len(os.Args) != 2 {
+		log.Fatal("Usage: bb_copy bb_copy.jsonnet")
+	}
+	var configuration bb_copy.ApplicationConfiguration
+	if err := util.UnmarshalConfigurationFromFile(os.Args[1], &configuration); err != nil {
+		log.Fatalf("Failed to read configuration from %s: %s", os.Args[1], err)
+	}
+
+	grpcClientFactory := grpc.NewBaseClientFactory(grpc.BaseClientDialer, nil, nil)
+	terminationContext, terminationCancel := context.WithCancel(context.Background())
+	var terminationGroup errgroup.Group
+
+	blobAccessCreator := blobstore_configuration.NewCASBlobAccessCreator(
+		grpcClientFactory,
+		int(configuration.MaximumMessageSizeBytes))
+	source, err := blobstore_configuration.NewBlobAccessFromConfiguration(
+		terminationContext,
+		&terminationGroup,
+		configuration.Source,
+		blobAccessCreator)
+	if err != nil {
+		log.Fatal("Failed to create source: ", err)
+	}
+	sink, err := blobstore_configuration.NewBlobAccessFromConfiguration(
+		terminationContext,
+		&terminationGroup,
+		configuration.Sink,
+		blobAccessCreator)
+	if err != nil {
+		log.Fatal("Failed to create sink: ", err)
+	}
+	replicator, err := blobstore_configuration.NewBlobReplicatorFromConfiguration(
+		configuration.Replicator,
+		source.BlobAccess,
+		sink,
+		blobstore_configuration.NewCASBlobReplicatorCreator(grpcClientFactory))
+	if err != nil {
+		log.Fatal("Failed to create replicator: ", err)
+	}
+	nestedReplicator := replication.NewNestedBlobReplicator(
+		replicator,
+		sink.DigestKeyFormat,
+		int(configuration.MaximumMessageSizeBytes))
+
+	ctx := context.Background()
+	instanceName, err := digest.NewInstanceName(configuration.InstanceName)
+	if err != nil {
+		log.Fatal("Invalid instance name: ", err)
+	}
+
+	// Enqueue objects for replication.
+	for i, action := range configuration.Actions {
+		actionDigest, err := instanceName.NewDigestFromProto(action)
+		if err != nil {
+			log.Fatal("Invalid action digest at index %d: %s", i, err)
+		}
+		nestedReplicator.EnqueueAction(actionDigest)
+	}
+	for i, blob := range configuration.Blobs {
+		blobDigest, err := instanceName.NewDigestFromProto(blob)
+		if err != nil {
+			log.Fatal("Invalid blob digest at index %d: %s", i, err)
+		}
+		if err := replicator.ReplicateMultiple(ctx, blobDigest.ToSingletonSet()); err != nil {
+			log.Fatalf("Failed to schedule replication of blob with digest %#v: %s", blobDigest.String(), err)
+		}
+	}
+	for i, directory := range configuration.Directories {
+		directoryDigest, err := instanceName.NewDigestFromProto(directory)
+		if err != nil {
+			log.Fatal("Invalid directory digest at index %d: %s", i, err)
+		}
+		nestedReplicator.EnqueueDirectory(directoryDigest)
+	}
+	for i, tree := range configuration.Trees {
+		treeDigest, err := instanceName.NewDigestFromProto(tree)
+		if err != nil {
+			log.Fatalf("Invalid tree digest at index %d: %s", i, err)
+		}
+		nestedReplicator.EnqueueTree(treeDigest)
+	}
+
+	// Perform replication of nested objects.
+	replicationGroup, replicationCtx := errgroup.WithContext(ctx)
+	for i := int32(0); i < configuration.TraversalConcurrency; i++ {
+		replicationGroup.Go(func() error {
+			return nestedReplicator.Replicate(replicationCtx)
+		})
+	}
+	if err := replicationGroup.Wait(); err != nil {
+		log.Fatal("Failed to replicate nested objects: ", err)
+	}
+
+	// Finalize.
+	log.Print("Copying completed. Shutting down storage gracefully.")
+	terminationCancel()
+	if err := terminationGroup.Wait(); err != nil {
+		log.Fatal("Failed to shut down storage gracefully: ", err)
+	}
+}
