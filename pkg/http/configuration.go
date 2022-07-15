37a5fd88426e06320d8671db5c1ab0961f6f2e91
Ed Schouten <eschouten@apple.com>
2022-07-15 09:34:23 +0200

    Enable TCP keepalives on HTTP clients
    http.DefaultTransport seems to use a 30-second connect timeout and
    keepalive. As we need to instantiate our own http.Transport (for custom
    proxy support), those defaults seem to get lost. Looking at the net/http
    source code, it means that a zero Dialer object gets used, meaning there
    are no timeouts/keepalives.
    
    This change adds a Dialer with the same options as used by
    http.DefaultTransport. A documented side-effect is that if you set
    DialContext, you cause it to fall back to only using HTTP/1.1. That's
    why we also need to set ForceAttemptHTTP2, just like
    http.DefaultTransport does.

--- pkg/http/configuration.go
+++ pkg/http/configuration.go
@@ -1,8 +1,10 @@
 package http
 
 import (
+	"net"
 	"net/http"
 	"net/url"
+	"time"
 
 	pb "github.com/buildbarn/bb-storage/pkg/proto/configuration/http"
 	"github.com/buildbarn/bb-storage/pkg/util"
@@ -16,7 +18,12 @@ func NewRoundTripperFromConfiguration(configuration *pb.ClientConfiguration) (ht
 		return nil, err
 	}
 	defaultTransport := http.Transport{
-		TLSClientConfig: tlsConfig,
+		DialContext: (&net.Dialer{
+			Timeout:   30 * time.Second,
+			KeepAlive: 30 * time.Second,
+		}).DialContext,
+		ForceAttemptHTTP2: true,
+		TLSClientConfig:   tlsConfig,
 	}
 	if proxyURL := configuration.GetProxyUrl(); proxyURL != "" {
 		parsedProxyURL, err := url.Parse(proxyURL)
