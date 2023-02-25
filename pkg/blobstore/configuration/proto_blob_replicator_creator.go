a133621fd5ea7eeaf15e131b8d72d0c135defdff
Ed Schouten <eschouten@apple.com>
2023-02-25 18:05:22 +0100

    Remove *BlobReplicatorCreator
    These were only added for symmetry with NewCASBlobReplicatorCreator().
    That function is used by bb_replicator to create a bare BlobReplicator
    to expose over gRPC.
    
    For the other data stores no such use case exists, so let's remove them.
    It looks like we forgot to add one for the File System Access Cache
    (FSAC) and nobody cared.

--- pkg/blobstore/configuration/proto_blob_replicator_creator.go
+++ pkg/blobstore/configuration/proto_blob_replicator_creator.go
@@ -14,20 +14,3 @@ type protoBlobReplicatorCreator struct{}
 func (brc protoBlobReplicatorCreator) NewCustomBlobReplicator(configuration *pb.BlobReplicatorConfiguration, source blobstore.BlobAccess, sink BlobAccessInfo) (replication.BlobReplicator, error) {
 	return nil, status.Error(codes.InvalidArgument, "Configuration did not contain a supported replicator")
 }
-
-var (
-	// ACBlobReplicatorCreator is a BlobReplicatorCreator that can be
-	// provided to NewBlobReplicatorFromConfiguration() to construct a
-	// BlobReplicator that is suitable for replicating Action Cache objects.
-	ACBlobReplicatorCreator BlobReplicatorCreator = protoBlobReplicatorCreator{}
-	// ICASBlobReplicatorCreator is a BlobReplicatorCreator that can be
-	// provided to NewBlobReplicatorFromConfiguration() to construct a
-	// BlobReplicator that is suitable for replicating Indirect Content
-	// Addressable Storage objects.
-	ICASBlobReplicatorCreator BlobReplicatorCreator = protoBlobReplicatorCreator{}
-	// ISCCBlobReplicatorCreator is a BlobReplicatorCreator that can
-	// be provided to NewBlobReplicatorFromConfiguration() to
-	// construct a BlobReplicator that is suitable for replicating
-	// Initial Size Class Cache objects.
-	ISCCBlobReplicatorCreator BlobReplicatorCreator = protoBlobReplicatorCreator{}
-)
