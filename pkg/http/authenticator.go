meroton/buildbarn-bb-storage

Implement remote authenticator and authorizer
The authenticate and authorize tasks can now be sent remotely over gRPC
to an external service. This way, custom authentication and
authorization does not require a modified builds of the Buildbarn
components.

To avoid spamming the remote service with calls for every REv2 request
and keep the latency low, the verdicts, both allow and deny, are cached
for a duration specified in the response from the remote service.

pkg/http/authenticator.go
@@ -125,6 +125,12 @@ func NewAuthenticatorFromConfiguration(policy *configuration.AuthenticationPolic
 			return nil, err
 		}
 		return NewAcceptHeaderAuthenticator(base, policyKind.AcceptHeader.MediaTypes), nil
+	case *configuration.AuthenticationPolicy_Remote:
+		backend, err := grpc.NewRequestHeadersAuthenticatorFromConfiguration(policyKind.Remote.Backend, grpcClientFactory)
+		if err != nil {
+			return nil, err
+		}
+		return NewRemoteHTTPRequestAuthenticator(backend, policyKind.Remote.Headers)
 	default:
 		return nil, status.Error(codes.InvalidArgument, "Configuration did not contain an authentication policy type")
 	}

buildbarn/bb-storage

pkg/http/authenticator.go
@@ -125,6 +125,12 @@ func NewAuthenticatorFromConfiguration(policy *configuration.AuthenticationPolic
 			return nil, err
 		}
 		return NewAcceptHeaderAuthenticator(base, policyKind.AcceptHeader.MediaTypes), nil
+	case *configuration.AuthenticationPolicy_Remote:
+		backend, err := grpc.NewRequestHeadersAuthenticatorFromConfiguration(policyKind.Remote.Backend, grpcClientFactory)
+		if err != nil {
+			return nil, err
+		}
+		return NewRequestHeadersAuthenticator(backend, policyKind.Remote.Headers)
 	default:
 		return nil, status.Error(codes.InvalidArgument, "Configuration did not contain an authentication policy type")
 	}
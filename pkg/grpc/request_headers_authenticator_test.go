7339d98795b67240ee900b05c72f307c601fc984
Fredrik Medley <fredrik@meroton.com>
2024-11-20 13:57:03 +0100

    Implement remote authenticator and authorizer
    The authenticate and authorize tasks can now be sent remotely over gRPC
    to an external service. This way, custom authentication and
    authorization does not require a modified builds of the Buildbarn
    components.
    
    To avoid spamming the remote service with calls for every REv2 request
    and keep the latency low, the verdicts, both allow and deny, are cached
    for a duration specified in the response from the remote service.

---
+++ pkg/grpc/request_headers_authenticator_test.go
@@ -0,0 +1,76 @@
+package grpc_test
+
+import (
+	"context"
+	"testing"
+
+	"github.com/buildbarn/bb-storage/internal/mock"
+	"github.com/buildbarn/bb-storage/pkg/auth"
+	bb_grpc "github.com/buildbarn/bb-storage/pkg/grpc"
+	auth_pb "github.com/buildbarn/bb-storage/pkg/proto/auth"
+	"github.com/buildbarn/bb-storage/pkg/testutil"
+	"github.com/stretchr/testify/require"
+	"go.uber.org/mock/gomock"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/metadata"
+	"google.golang.org/grpc/status"
+	"google.golang.org/protobuf/types/known/structpb"
+)
+
+func TestRequestHeadersAuthenticator(t *testing.T) {
+	ctrl, ctx := gomock.WithContext(context.Background(), t)
+	md := metadata.New(
+		map[string]string{
+			"Authorization": "token",
+			"OtherHeader":   "Other",
+			"UnusedHeader":  "DontUseMe",
+		},
+	)
+	md.Append("OtherHeader", "Other2")
+	grpcCtx := metadata.NewIncomingContext(ctx, md)
+	backend := mock.NewMockRequestHeadersAuthenticator(ctrl)
+
+	t.Run("BackendSuccess", func(t *testing.T) {
+		backend.EXPECT().Authenticate(
+			grpcCtx, map[string][]string{
+				"Authorization": {"token"},
+				"OtherHEADER":   {"Other", "Other2"},
+				// MissingHeader is not part of the request.
+				// UnusedHeader should not be passed.
+			},
+		).Return(auth.MustNewAuthenticationMetadataFromProto(&auth_pb.AuthenticationMetadata{
+			Public: structpb.NewStringValue("You're totally who you say you are"),
+		}), nil)
+
+		authenticator := bb_grpc.NewRequestHeadersAuthenticator(
+			backend,
+			[]string{
+				"Authorization",
+				"OtherHEADER",
+				"MissingHeader",
+			},
+		)
+		metadata, err := authenticator.Authenticate(grpcCtx)
+		require.NoError(t, err)
+		require.Equal(t, map[string]any{
+			"public": "You're totally who you say you are",
+		}, metadata.GetRaw())
+	})
+
+	t.Run("BackendFailure", func(t *testing.T) {
+		headerKeys := []string{}
+		backend.EXPECT().Authenticate(
+			grpcCtx, map[string][]string{},
+		).Return(nil, status.Error(codes.Unavailable, "Server offline"))
+
+		authenticator := bb_grpc.NewRequestHeadersAuthenticator(
+			backend,
+			headerKeys,
+		)
+		_, err := authenticator.Authenticate(grpcCtx)
+		testutil.RequireEqualStatus(
+			t,
+			status.Error(codes.Unavailable, "Server offline"),
+			err)
+	})
+}
