e9454da639dc68ac76dc301f3f8f0d7aa4177c4f
Ed Schouten <eschouten@apple.com>
2026-01-26 22:37:23 +0100

    Upgrade all Go dependencies

--- tools/deps.go
+++ tools/deps.go
@@ -2,7 +2,7 @@ package tools
 
 import (
 	// Used by //:buildifier.
-	_ "github.com/bazelbuild/buildtools/buildifier/cmd"
+	_ "github.com/bazelbuild/buildtools/buildifier"
 	// Used by CI.
 	_ "golang.org/x/lint"
 	// Used by CI.
