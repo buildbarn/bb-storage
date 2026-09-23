17c5a35253845ccb8a3b521f2f2447bcd75e785c
Fredrik Medley <fredrik@meroton.com>
2026-09-23 15:53:51 +0200

    Resurrect GitHub workflows generation
    When all file generation and formatting was moved to //tools:reformat,
    in commit 5c8395f3cdf64f1e048ec04986349e80a7b31c94 from 2025-10-08, the
    verification of .github/workflows/ was accidentally removed. Fix that.

--- tools/reformat.sh
+++ tools/reformat.sh
@@ -75,3 +75,6 @@ if git grep -q '^[[:space:]]*//go:embed '; then
     fi
   done
 fi
+
+# Generate GitHub workflows.
+bazel build //tools/github_workflows && cp bazel-bin/tools/github_workflows/*.yaml .github/workflows
